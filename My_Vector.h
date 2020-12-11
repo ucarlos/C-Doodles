29e24b7662701307a03c051c9f78413a0a80cc0e
Ulysses Carlos <ulysses_carlos@protonmail.com>
2020-12-11 17:32:06 -0500

    Reverted My_Vector.h

--- My_Vector.h
+++ My_Vector.h
@@ -14,49 +14,28 @@
 #include <initializer_list>
 #include <memory>
 #include <stdexcept>
-
-template<class T, class A> struct my_vector_base{
-    A alloc; // Allocator
-    T* elem{nullptr}; // Start of allocation
-
-    int sz{0};
-    int space{0};
-    
-    // Default constructor
-    my_vector_base()= default;
-    
-    my_vector_base(const A &a, int n): alloc{a}, elem{alloc.allocate(n)},
-				    sz{n}, space{n} { }
-
-    ~my_vector_base(){ alloc.deallocate(elem, space); }
-
-};
-
-template<class T, class A = std::allocator<T>>
-class my_vector : private my_vector_base<T, A>{
+template<class T, class A = std::allocator<T>> class my_vector{
 public:
     // Default constructor
-    my_vector(){
-	    this->elem = nullptr;
-	    this->sz = 0;
-	    this->space = 0;
-    }
+    my_vector(): elem{nullptr}, sz{0}, space{0} { }
     
-    explicit my_vector(int size);
+    explicit my_vector(int s): elem{new T[s]}, sz{s}, space{s} {
+	for (int i = 0; i < sz; i++) elem[i] = 0;
+    }
     
     my_vector(const my_vector&); // Copy constructor
     my_vector& operator=(const my_vector&); // Copy assignment
 
     my_vector(std::initializer_list<T> lst); // Initializer list constructor
-    my_vector(my_vector&&) noexcept ; // Move constructor
-    my_vector& operator=(my_vector&&) noexcept ; // Move assignment
+    my_vector(my_vector&&); // Move constructor
+    my_vector& operator=(my_vector&&); // Move assignment
 
-    T& operator[](int n) { return this->elem[n]; }
-    const T& operator[](int n) const { return this->elem[n]; }
+    T& operator[](int n) { return elem[n]; }
+    const T& operator[](int n) const { return elem[n]; }
     
     T& at(int n);
-    int size() const { return this->sz; }
-    int capacity() const { return this->space; }
+    int size() const { return sz; }
+    int capacity() const { return space; }
 
     void resize(int new_size, T val = T());
     void push_back(const T& val);
@@ -64,11 +43,11 @@ public:
     
     // Destructor
     ~my_vector();
-// private:
-//     A alloc;
-//     T *elem{nullptr};
-//     int sz{0};
-//     int space{0};
+private:
+    A alloc;
+    T *elem{nullptr};
+    int sz{0};
+    int space{0};
 
 };
 
