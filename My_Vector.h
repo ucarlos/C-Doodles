e28892b11b0a3e293c6c32e739fe053b32f342b2
Ulysses Carlos <ulysses_carlos@protonmail.com>
2021-03-28 14:58:45 -0400

    Moved Implementation of My_Vector to My_Vector.h

--- My_Vector.h
+++ My_Vector.h
@@ -9,33 +9,34 @@
  * -----------------------------------------------------------------------------
  */
 
-#ifndef MY_VECTOR
-#define MY_VECTOR
+#pragma once
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
 #include <initializer_list>
 #include <memory>
 #include <stdexcept>
-template<class T, class A = std::allocator<T>> class my_vector{
+
+template<class T, class A = std::allocator<T>> class my_vector {
 public:
     // Default constructor
     my_vector(): elem{nullptr}, sz{0}, space{0} { }
     
-    explicit my_vector(int s): elem{new T[s]}, sz{s}, space{s} {
-	for (int i = 0; i < sz; i++) elem[i] = 0;
-    }
+    explicit my_vector(int s);
     
     my_vector(const my_vector&); // Copy constructor
     my_vector& operator=(const my_vector&); // Copy assignment
 
     my_vector(std::initializer_list<T> lst); // Initializer list constructor
-    my_vector(my_vector&&); // Move constructor
-    my_vector& operator=(my_vector&&); // Move assignment
+    my_vector(my_vector&&) noexcept ; // Move constructor
+    my_vector& operator=(my_vector&&) noexcept; // Move assignment
 
     T& operator[](int n) { return elem[n]; }
     const T& operator[](int n) const { return elem[n]; }
     
     T& at(int n);
-    int size() const { return sz; }
-    int capacity() const { return space; }
+    [[nodiscard]] int size() const { return sz; }
+    [[nodiscard]] int capacity() const { return space; }
 
     void resize(int new_size, T val = T());
     void push_back(const T& val);
@@ -50,5 +51,225 @@ private:
     int space{0};
 
 };
+
+//------------------------------------------------------------------------------
+// Vector Constructors
+//------------------------------------------------------------------------------
+
+// Explicit Constructor
+template<class T, class A> my_vector<T,A>::my_vector(int s) {
+	// Call Resize by default
+	resize(s);
+}
+
+
+
+template<class T, class A> my_vector<T,A>::my_vector(const my_vector<T,A> &vec) {
+	// Allocate the size of vec.size
+	T *p = alloc.allocate(vec.size());
+
+	// Now copy over the contents of vec:
+	for (int i = 0; i < vec.size(); i++) {
+		alloc.construct(&p[i], vec[i]);
+	}
+
+	sz = space = vec.size();
+	elem = p;
+}
+
+// template<class T, class A> my_vector<T, A>& my_vector<T,A>::operator=(const my_vector<T, A> &vec) {
+
+// }
+
+// template<class T, class A> my_vector<T, A>& my_vector<T, A>::operator=(const my_vector<T, A> &vec){
+//     if (this == &vec)
+// 	return *this;
+
+//     // If size of vec is smaller than space, destroy the current space and then
+//     if (vec.sz <= this->space){ //
+// 	// First, destroy elem:
+// 	for (int i = 0; i < this->sz; i++)
+// 	    this->alloc.destroy(&this->elem[i]);
+
+// 	for (int i = 0; i < vec.sz; i++)
+// 	    this->alloc.construct(&this->elem[i], vec.elem[i]);
+
+// 	this->sz = vec.sz;
+// 	return *this;
+
+//     }
+
+//     // Otherwise, if the sz is larger than the space,
+//     // allocate enough space, and then copy all data from vec
+//     // to the new elem.
+
+//     my_vector_base<T,A> b(this->alloc, vec.sz);
+//     std::uninitialized_copy(vec.elem, &vec.elem[vec.sz], b.elem);
+
+//     for (int i = 0; i < this->sz; i++)
+// 	this->alloc.destroy(&this->elem[i]);
+
+//     // Now swap representations:
+//     std::swap<my_vector_base<T,A>>(*this, b);
+//     // T *p = new T[vec.sz];
+//     // for (int i = 0; i < vec.sz; i++)
+//     // 	p[i] = vec.elem[i];
+
+//     // delete[] elem; // Deallocate old space
+//     // elem = p; // assign new pointer
+
+
+//     this->sz = this->space = vec.sz; // Set new size
+
+//     return *this; // Return a self-reference.
+// }
+
+
+// Copy assignment
+// template<class T, class A> my_vector<T, A>& my_vector<T, A>::operator=(const my_vector<T, A> &vec){
+//     if (this == &vec)
+// 		return *this;
+
+//     if (vec.sz <= space){
+// 		for (int i = 0; i < vec.sz; i++)
+// 			elem[i] = vec.elem[i];
+// 		this->sz = vec.sz;
+// 		return *this;
+//     }
+
+//     // Otherwise, if the sz is larger than the space,
+//     // allocate enough space, and then copy all data from vec
+//     // to the new elem.
+
+//     T *p = new T[vec.sz];
+//     for (int i = 0; i < vec.sz; i++)
+// 		p[i] = vec.elem[i];
+
+//     delete[] elem; // Deallocate old space
+//     elem = p; // assign new pointer
+//     this->sz = this->space = vec.sz; // Set new size
+
+//     return *this; // Return a self-reference.
+// }
+
+// Move constructor
+template<class T, class A> my_vector<T, A>::my_vector(my_vector<T, A> &&vec) noexcept {
+	sz = vec.sz;
+	space = vec.space;
+	elem = vec.elem;
+
+	// Now set sz and space to zero
+	vec.sz = vec.space = 0;
+	vec.elem = nullptr;
+
+}
+
+// Move assignment
+template<class T, class A> my_vector<T, A>& my_vector<T, A>::operator=(my_vector<T, A> &&vec) noexcept {
+	// Do nothing if vec is the same as this
+	if (this == &vec)
+		return *this;
+
+	for (int i = 0; i < sz; i++)
+		alloc.destroy(&elem[i]);
+
+	for (int i = 0; i < space; i++)
+		alloc.deallocate(&elem[i]);
+
+	elem = vec.elem;
+	sz = vec.sz;
+	space = vec.space;
+
+	vec.sz = vec.space = 0;
+	vec.elem = nullptr;
+	return *this;
+}
+
+// Initializer_list constructor
+template<class T, class A> my_vector<T, A>::my_vector(std::initializer_list<T> lst) {
+	T *p = alloc.allocate(lst.size());
+	this->space = this->sz = lst.size();
+
+	auto it  = lst.begin();
+
+	// for (unsigned int i = 0; i < lst.size(); i++)
+	// 	elem[i] = *it++;
+	for (unsigned int i = 0; i < lst.size(); i++)
+		alloc.construct(&p[i], *it++);
+
+	elem = p;
+
+}
+
+// Destructor
+template<class T, class A> my_vector<T,A>::~my_vector() {
+	// If elem is nullptr, do nothing
+	if (elem != nullptr) {
+
+		//First destroy all elements:
+		for (int i = 0; i < sz; i++)
+			alloc.destroy(&elem[i]);
+
+		// Now deallocate memory:
+		alloc.deallocate(elem, space);
+	}
+}
+
+//------------------------------------------------------------------------------
+// My_Vector member functions
+//------------------------------------------------------------------------------
+template<class T, class A> void my_vector<T, A>::resize(int new_size, T val) {
+	// Let reserve handle causes where the new_size is less than space
+	reserve(new_size);
+
+	// Initialize all new elements
+	// OLD:
+	// for (int i = sz; i < space; i++)
+	// 	elem[i] = 0;
+	// Construct
+
+	for (int i = sz; i < new_size; i++)
+		alloc.construct(&elem[i], val);
+
+	sz = new_size;
+}
+
+template<class T, class A> T& my_vector<T, A>::at(int n) {
+	if (!(0 <= n && n < sz))
+		throw std::runtime_error("Out of Range");
+
+	return elem[n];
+}
+
+template<class T, class A> void my_vector<T, A>::push_back(const T &val) {
+	// Make sure that we have enough space
+	if (!space)
+		reserve(8); // Reserve eight blocks
+	else if (sz == space)
+		reserve(2 * space); // Double space if at capacity
+
+	alloc.construct(&elem[sz], val);
+	sz++;
+
+}
+
+template<class T, class A> void my_vector<T,A>::reserve(int newalloc) {
+	if (newalloc <= space) return;
+
+	// Allocate new memory
+	T *p = alloc.allocate(newalloc);
+	// construct
+	for (int i = 0; i < sz; i++)
+		alloc.construct(&p[i], elem[i]);
+	// Deconstruct old memory
+	for (int i = 0; i < sz; i++)
+		alloc.destroy(&elem[i]);
+	// Deallocate old memory
+	alloc.deallocate(elem, space);
+
+	elem = p;
+	space = newalloc;
+
+}
 
 #endif
--- Test_MyVector.cc
+++ Test_MyVector.cc
@@ -1,9 +1,24 @@
-#include <iostream>
-#include "./My_Vector.h"
+/*
+ * -----------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 08/16/2020 at 08:32 PM
+ *
+ * My_Vector.cc
+ *
+ * -----------------------------------------------------------------------------
+ */
 
-int main(void){
+#include <iostream>
+#include "My_Vector.h"
+using namespace std;
+
+int main(){
     my_vector<double> test;
-    test.resize(100, 10.2);
-    test.resize(120);
+    test.push_back(12.1);
+	test.resize(30, 10.2);
+
+	for (int i = 0; i < test.size(); i++) {
+		cout << "Index " << i << " " << test.at(i) << endl;
+	}
+    //test.resize(120);
 
 }
