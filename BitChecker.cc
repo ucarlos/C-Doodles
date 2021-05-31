48c8d9c612e8392a40a12342c57649a1b970d5e9
Ulysses Carlos <ulysses_carlos@protonmail.com>
2021-05-31 00:17:01 -0400

    Extended BitChecker.cc to uint64

--- BitChecker.cc
+++ BitChecker.cc
@@ -8,6 +8,7 @@
  */
 
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
 int get_set_bits(unsigned char value) {
@@ -21,12 +22,31 @@ int get_set_bits(unsigned char value) {
 	return count;
 }
 
+int get_set_bits(uint64_t val) {
+	int count = 0;
+
+	// Only set bit is the most significant bit
+	uint64_t shift = 0x8000000000000000;
+	size_t end = sizeof(uint64_t) * 8;
+
+	// Check each bit starting from most significant to least significant
+	for (size_t i = 0; i < end; i++, shift >>= 1) {
+		uint64_t temp = (val & shift);
+		if (temp)
+			count++;
+	}
+	
+	return count;
+
+}
+
 int main() {
 	//unsigned char val = 0x71;
-	unsigned char val;
+	uint64_t val;
 	cout << "Please enter a value: ";
 	cin >> val;
+	// int result = get_set_bits(static_cast<uint64_t>(val));
 	int result = get_set_bits(val);
-
+	
 	cout << "Number of set bits: " << result << endl;
 }
