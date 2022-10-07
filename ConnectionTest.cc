5e522678348a0817f41d46f95f4a31258d97697b
Ulysses Carlos <ulysses_carlos@protonmail.com>
2022-10-06 21:15:09 -0400

    Initial Commit

---
+++ ConnectionTest.cc
@@ -0,0 +1,15 @@
+/*
+ * ------------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 10/06/2022 at 09:12 PM
+ *
+ * ConnectionTest.cc
+ * Just a simple connection test to a psql database.
+ * ------------------------------------------------------------------------------
+ */
+
+#include <iostream>
+
+int main(void) {
+
+
+}
