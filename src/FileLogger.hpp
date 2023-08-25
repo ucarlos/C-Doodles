30dffc057c4393b94c19aef8961734efa12e838c
Ulysses Carlos <ulysses_carlos@protonmail.com>
2023-08-24 22:17:59 -0400

    Created Logger.hpp and FileLogger.hpp to finally have a proper logging class.

---
+++ src/FileLogger.hpp
@@ -0,0 +1,35 @@
+/*
+ * ------------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 08/23/2023 at 10:36 PM
+ *
+ * FileLogger.hpp
+ * Simple Filelogger class.
+ * ------------------------------------------------------------------------------
+ */
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <filesystem>
+
+const std::string DEFAULT_LOG_FILE_LOCATION = "DefaultLogFile.txt";
+
+class FileLogger {
+public:
+	FileLogger() =default;
+
+    FileLogger(const std::string filename) : _filename{filename} {
+        _logger = spdlog::basic_logger_mt("file_logger", filename);
+    }
+
+    FileLogger(const std::filesystem::path file_path) : _filename{file_path.string()} {
+        _logger = spdlog::basic_logger_mt("file_logger", _filename);
+    }
+
+    std::shared_ptr<spdlog::logger>& logger() { return this->_logger; }
+
+
+private:
+    std::string _filename;
+    std::shared_ptr<spdlog::logger> _logger = spdlog::basic_logger_mt("file_logger",
+                                                                      std::filesystem::current_path() / DEFAULT_LOG_FILE_LOCATION);
+};
---
+++ src/Logger.hpp
@@ -0,0 +1,89 @@
+/*
+ * ------------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 08/23/2023 at 10:54 PM
+ *
+ * Logger.hpp
+ * A simple logger class that wraps around spdlog.
+ * ------------------------------------------------------------------------------
+ */
+
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <spdlog/spdlog.h>
+#include <spdlog/details/log_msg.h>
+#include <spdlog/sinks/ansicolor_sink.h>
+#include <unordered_map>
+#include <unordered_set>
+
+class Logger {
+public:
+    Logger() = default;
+    explicit Logger(const std::string logger_name) : _logger_name{logger_name} { }
+
+    spdlog::sinks::ansicolor_stderr_sink<spdlog::details::console_mutex>& logger() { return _logger; }
+    void debug(const std::string &message) {
+        if (!log_level_is_at_least(spdlog::level::debug))
+            return;
+
+        this->_log_message = spdlog::details::log_msg{this->_logger_name, spdlog::level::debug, message};
+        _logger.log(_log_message);
+    }
+
+    void info(const std::string &message) {
+        if (!log_level_is_at_least(spdlog::level::info))
+            return;
+
+        this->_log_message = spdlog::details::log_msg{this->_logger_name, spdlog::level::info, message};
+        this->_logger.log(this->_log_message);
+    }
+
+    void error(const std::string &message) {
+        if (!log_level_is_at_least(spdlog::level::err))
+            return;
+
+        this->_log_message = spdlog::details::log_msg{this->_logger_name, spdlog::level::err, message};
+        this->_logger.log(this->_log_message);
+    }
+
+    void critical(const std::string &message) {
+        if (!log_level_is_at_least(spdlog::level::critical))
+            return;
+
+        this->_log_message = spdlog::details::log_msg{this->_logger_name, spdlog::level::critical, message};
+        this->_logger.log(this->_log_message);
+    }
+
+    void warn(const std::string &message) {
+        if (!log_level_is_at_least(spdlog::level::warn))
+            return;
+
+        this->_log_message = spdlog::details::log_msg{this->_logger_name, spdlog::level::warn, message};
+        this->_logger.log(this->_log_message);
+    }
+
+
+private:
+    spdlog::details::log_msg _log_message{};
+    std::string _logger_name{"logger"};
+    spdlog::sinks::ansicolor_stderr_sink_mt _logger = spdlog::sinks::ansicolor_stderr_sink_mt();
+
+    bool log_level_is_at_least(const spdlog::level::level_enum required_level) {
+        // This is done because the log levels aren't in sequential order (ie. I can't do log_level < debug)
+        // the enum isn't ordered by intensity current_level; I know it's disgusting.
+		const std::unordered_map<spdlog::level::level_enum, std::unordered_set<spdlog::level::level_enum>> allowed_level = {
+			{spdlog::level::info, {spdlog::level::info, spdlog::level::debug, spdlog::level::warn, spdlog::level::err, spdlog::level::critical}},
+            {spdlog::level::debug, {spdlog::level::debug, spdlog::level::warn, spdlog::level::err, spdlog::level::critical}},
+            {spdlog::level::warn, {spdlog::level::warn, spdlog::level::err, spdlog::level::critical}},
+            {spdlog::level::err, {spdlog::level::err, spdlog::level::critical}},
+            {spdlog::level::critical, {spdlog::level::critical}}
+		};
+
+        spdlog::level::level_enum current_level = this->_logger.level();
+        return allowed_level.at(required_level).find(current_level) != allowed_level.at(required_level).end();
+    }
+
+};
+
+#endif
